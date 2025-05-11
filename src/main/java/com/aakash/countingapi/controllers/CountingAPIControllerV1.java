850b2cc8741ee1a71b15baa88d92c3b31c371657
aakash_roy <aakash.roy@franconnect.com>
2025-05-11 14:09:33 +0530

    removed error import

--- src/main/java/com/aakash/countingapi/controllers/CountingAPIControllerV1.java
+++ src/main/java/com/aakash/countingapi/controllers/CountingAPIControllerV1.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Generated;
 import java.util.List;
 
 @RestController
